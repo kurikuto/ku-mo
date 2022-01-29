019f79d3eddb0318832a0b060ae8e805178b8732
kurikuto <k.ab.kurikuto@gmail.com>
2022-01-29 14:43:58 +0900

    install capacitor

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,6 +106,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-hook',
+          title: 'Fun',
+          to: '/fun'
         }
       ],
       miniVariant: false,
